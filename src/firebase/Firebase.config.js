5dbf9d8da5955e30580da4b84c6e8f3ce166c7a2
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-02 21:23:55 +0600

    first commit

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,15 @@
+
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDgRGhx4oBgU1uIJg8R8GSQak7FI6mEw_k",
+//   authDomain: "practice-module-51-8a9fa.firebaseapp.com",
+//   projectId: "practice-module-51-8a9fa",
+//   storageBucket: "practice-module-51-8a9fa.appspot.com",
+//   messagingSenderId: "197580112932",
+//   appId: "1:197580112932:web:f19f10193673b5813904b1"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+
